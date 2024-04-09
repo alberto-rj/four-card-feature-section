39ed026e203d16bd263f46c4b7654f9fa7baa0c6
Alberto Raúl José <josywonder2@gmail.com>
2024-04-09 13:38:50 +0100

    Finished project

---
+++ README.md
@@ -0,0 +1,66 @@
+# Frontend Mentor - Four card feature section solution
+
+This is a solution to the [Four card feature section challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+
+### Screenshot
+
+<details>
+  <summary>Mobile version</summary>
+  <img alt="Mobile version screenshot" src="screenshot/screenshot-mobile.png">
+</details>
+
+<details>
+  <summary>Desktop version</summary>
+  <img alt="Desktop version screenshot" src="screenshot/screenshot-desktop.png">
+</details>
+
+### Links
+
+- [Solution](https://your-solution-url.com)
+- [Live Site](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- CSS Grid
+- Mobile-first workflow
+
+### What I learned
+
+I learned the following CSS grid properties:
+
+- `justify-items`
+- `justify-self`
+
+### Useful resources
+
+- [A Complete Guide to CSS Grid](https://css-tricks.com/snippets/css/complete-guide-grid/) - This is an amazing guide from [*Chris House*](https://css-tricks.com/author/caiman/) which helped me understand how to use the CSS Grid. I'd recommend it to anyone still learning this concept.
+
+## Author
+
+- Github - [Alberto Raúl José](https://github.com/alberto-rj)
+- Frontend Mentor - [@alberto-rj](https://www.frontendmentor.io/profile/alberto-rj)
+- Twitter - [@albertorauljose](https://www.twitter.com/albertorauljose)
---
+++ js/animation.js
@@ -0,0 +1,116 @@
+const intro = document.querySelector('.intro');
+const supervidorCard = document.querySelector('.card--supervisor');
+const teamBuilderCard = document.querySelector('.card--team-builder');
+const karmaCard = document.querySelector('.card--karma');
+const calculatorCard = document.querySelector('.card--calculator');
+
+const animatableElements = [
+  intro, 
+  supervidorCard, 
+  teamBuilderCard, 
+  karmaCard, 
+  calculatorCard
+];
+const animationClasses = [
+  'translate-from-top', 
+  'translate-from-right', 
+  'translate-from-bottom',
+  'translate-from-left',
+  'translate-delay-1',
+  'translate-delay-2',
+  'translate-delay-3',
+  'translate-delay-4',
+  'translate-delay-5',
+  'translate-delay-6',
+  'translate-delay-7',
+  'translate-delay-8',
+  'translate-delay-9',
+  'translate-delay-10'
+];
+
+const DEVICE_MOBILE = 0;
+const DEVICE_TABLET = 1;
+const DEVICE_DESKTOP = 2;
+
+let currentDevice = null;
+
+const isMobileDevice = () => window.matchMedia('screen and (max-width: 53.75rem)').matches;
+
+const isTabletDevice = () => window.matchMedia('(min-width: 53.75rem) and (max-width: 64rem)').matches;
+
+const isDesktopDevice = () => window.matchMedia('(min-width: 64rem)').matches;
+
+const getDevice = () => {
+  if (isDesktopDevice()) {
+    return DEVICE_DESKTOP;
+  } else if (isTabletDevice()) {
+    return DEVICE_TABLET;
+  } else {
+    return DEVICE_MOBILE;
+  }
+};
+
+const removeAnimationClasses = () => {
+  animatableElements.forEach(node => {
+    animationClasses.forEach(className => node.classList.remove(className));
+  });
+};
+
+const updateAnimation = device => {
+  if (currentDevice === device) return;
+
+  removeAnimationClasses();
+  
+  intro.classList.add('translate-from-left');
+  
+  switch (device) {
+    case DEVICE_DESKTOP:
+      supervidorCard.classList.add('translate-from-left');
+      supervidorCard.classList.add('translate-delay-1');
+
+      calculatorCard.classList.add('translate-from-right');
+      calculatorCard.classList.add('translate-delay-1');
+
+      teamBuilderCard.classList.add('translate-from-top');
+      teamBuilderCard.classList.add('translate-delay-2');
+
+      karmaCard.classList.add('translate-from-bottom');
+      karmaCard.classList.add('translate-delay-2');
+    break;
+
+    case DEVICE_TABLET:
+      supervidorCard.classList.add('translate-from-left');
+      supervidorCard.classList.add('translate-delay-1');
+
+      karmaCard.classList.add('translate-from-left');
+      karmaCard.classList.add('translate-delay-1');
+
+      teamBuilderCard.classList.add('translate-from-right');
+      teamBuilderCard.classList.add('translate-delay-2');
+
+      calculatorCard.classList.add('translate-from-right');
+      calculatorCard.classList.add('translate-delay-2');
+    break;
+
+    default:
+      supervidorCard.classList.add('translate-from-left');
+      supervidorCard.classList.add('translate-delay-1');
+      
+      calculatorCard.classList.add('translate-from-left');
+      calculatorCard.classList.add('translate-delay-2');
+      
+
+      teamBuilderCard.classList.add('translate-from-left');
+      teamBuilderCard.classList.add('translate-delay-3');
+
+      karmaCard.classList.add('translate-from-left');
+      karmaCard.classList.add('translate-delay-4');
+    break;
+  }
+  currentDevice = device;
+};
+
+const setAnimation = () => updateAnimation(getDevice());
+
+window.addEventListener('load', setAnimation);
+window.addEventListener('resize', setAnimation);
